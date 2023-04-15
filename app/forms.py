02572db44bd6278e23cae058ebd9d23c0b38d732
Ernesto <jorge.cywdt@gmail.com>
2023-04-14 19:28:15 -0500

    Notas para entender mejor el codigo

--- app/forms.py
+++ app/forms.py
@@ -1,22 +1,21 @@
 from django import forms
 
-LISTA_CURSOS=(
-    ("Python","Python"),
-    ("Django","Django"),
-    ("PHP","PHP"),
-    ("SqlServer","SqlServer"),
-    ("Oracle","Oracle"),
-    ("JavaScript","JavaScript"),
+# Se puede usar tuplas o listas
+LISTA_CURSOS = (
+    ("Python", "Python"),
+    ("Django", "Django"),
+    ("PHP", "PHP"),
+    ("SqlServer", "SqlServer"),
+    ("Oracle", "Oracle"),
+    ("JavaScript", "JavaScript"),
 )
 
-# clase para definir datos del alumno
 class Alumno(forms.Form):
-    #definir los datos del alumno
-    codigo=forms.CharField()
-    nombre=forms.CharField()
-    curso=forms.ChoiceField(choices=LISTA_CURSOS)
-    parcial=forms.IntegerField()
-    final=forms.IntegerField()
-    practica=forms.IntegerField()
-    
-    
+    # Definir los datos de la clase (inputs del formulario)
+    # Si no se especifica lo contrario, todos los inputs seran required=TRUE por defecto
+    codigo = forms.CharField()
+    nombre = forms.CharField()
+    curso = forms.ChoiceField(choices=LISTA_CURSOS)
+    parcial = forms.IntegerField()
+    final = forms.IntegerField()
+    practica = forms.IntegerField()
